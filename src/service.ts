2b0b89ecb761d5f56feaaa163de900378314ed2a
omarfarukee <umarfaruque1920@gmail.com>
2023-06-04 08:08:10 +0600

    cors setup done

---
+++ src/service.ts
@@ -0,0 +1,23 @@
+import mongoose = require('mongoose');
+import config from "./config/index";
+import app from "./app";
+// main().catch(err => console.log(err));
+
+async function main() {
+ 
+
+  try {
+     await mongoose.connect(config.dataBase_url as string);
+     console.log('database connected');
+
+     app.listen(config.port, () => {
+        console.log(`application app listening on port ${config.port}`)
+      })
+
+  } 
+  catch (error) {
+    console.log('fail to connect');
+  }
+
+}
+main()
