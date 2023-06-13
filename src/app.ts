54834ae841825936df7ef042c6f3f0ff9560a034
omarfarukee <umarfaruque1920@gmail.com>
2023-06-13 08:08:54 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express,{Application, Request , Response} from "express"
-import cors from "cors"
-const app : Application = express()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 app.use(cors())
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req : Request , res : Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
--- src/service.ts
+++ src/service.ts
@@ -1,23 +1,18 @@
-import mongoose = require('mongoose');
-import config from "./config/index";
-import app from "./app";
+import mongoose = require('mongoose')
+import config from './config/index'
+import app from './app'
 // main().catch(err => console.log(err));
 
 async function main() {
- 
-
   try {
-     await mongoose.connect(config.dataBase_url as string);
-     console.log('database connected');
+    await mongoose.connect(config.dataBase_url as string)
+    console.log('database connected')
 
-     app.listen(config.port, () => {
-        console.log(`application app listening on port ${config.port}`)
-      })
-
-  } 
-  catch (error) {
-    console.log('fail to connect');
+    app.listen(config.port, () => {
+      console.log(`application app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('fail to connect')
   }
-
 }
 main()
